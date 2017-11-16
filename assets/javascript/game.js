79358e58062b62e5fe4dfe3128469083db196e04
Kate <kate.stark13@sbcglobal.net>
2017-11-15 20:58:28 -0600

    add changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+ // VARIABLES==========================================================
+
+    var words = [
+        "wand",
+        "Harry",
+        "hogwarts",
+        "butterbeer",
+        "muggle"
+        ];
+ 
+
+      // Pick a random word
+      var word=words[Math.floor(Math.random()*words.length)];
+
+
+      // FUNCTIONS
+      // ==============================================================================
+     
